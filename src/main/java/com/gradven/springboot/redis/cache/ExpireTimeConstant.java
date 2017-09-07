bbf90767164da107f05fd0c4ca533c9b89526c79
刘航军 <liuhangjun@huacemedia.com>
2017-09-07 10:39:13 +0800

    时间常量接口Long改为long

--- src/main/java/com/gradven/springboot/redis/cache/ExpireTimeConstant.java
+++ src/main/java/com/gradven/springboot/redis/cache/ExpireTimeConstant.java
@@ -5,50 +5,50 @@ package com.gradven.springboot.redis.cache;
  */
 public interface ExpireTimeConstant {
     
-    Long NONE = 0L; // "无固定期限"
+    long NONE = 0L; // "无固定期限"
     
     
-    Long ONE_SEC = 1L; // "1秒钟"
+    long ONE_SEC = 1L; // "1秒钟"
     
     
-    Long FIVE_SEC = 5L;  //  "5秒钟"
+    long FIVE_SEC = 5L;  //  "5秒钟"
     
-    Long TEN_SEC = 10L;  //  "10秒钟"
-    
-    
-    Long HALF_A_MIN = 30L;  //  "30秒钟"
+    long TEN_SEC = 10L;  //  "10秒钟"
     
     
-    Long ONE_MIN = 60L;  //  "1分钟"
+    long HALF_A_MIN = 30L;  //  "30秒钟"
     
     
-    Long FIVE_MIN = 5 * 60L;  //  "5分钟"
+    long ONE_MIN = 60L;  //  "1分钟"
     
     
-    Long TEN_MIN = 10 * 60L;  //  "10分钟"
+    long FIVE_MIN = 5 * 60L;  //  "5分钟"
     
     
-    Long TWENTY_MIN = 20 * 60L;  //  "20分钟"
+    long TEN_MIN = 10 * 60L;  //  "10分钟"
     
     
-    Long HALF_AN_HOUR = 30 * 60L;  //  "30分钟"
+    long TWENTY_MIN = 20 * 60L;  //  "20分钟"
     
     
-    Long ONE_HOUR = 60 * 60L;  //  "1小时"
+    long HALF_AN_HOUR = 30 * 60L;  //  "30分钟"
     
     
-    Long SIX_HOUR = 6 * 60 * 60L;  //  "6小时"
+    long ONE_HOUR = 60 * 60L;  //  "1小时"
     
     
-    Long TWELVE_HOUR = 12 * 60 * 60L;  //  "12小时"
+    long SIX_HOUR = 6 * 60 * 60L;  //  "6小时"
     
     
-    Long ONE_DAY = 24 * 60 * 60L;  //  "1天"
+    long TWELVE_HOUR = 12 * 60 * 60L;  //  "12小时"
     
     
-    Long ONE_MON = 30 * 24 * 60 * 60L;  //  "1个月"
+    long ONE_DAY = 24 * 60 * 60L;  //  "1天"
     
     
-    Long ONE_YEAR = 365 * 24 * 60 * 60L;  //  "1年"
+    long ONE_MON = 30 * 24 * 60 * 60L;  //  "1个月"
+    
+    
+    long ONE_YEAR = 365 * 24 * 60 * 60L;  //  "1年"
     
 }
